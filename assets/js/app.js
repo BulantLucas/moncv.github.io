27c4e00ec3f795537ff8536d8ab33ca470173534
BulantLucas <bulant.lucas@gmail.com>
2021-07-01 15:04:15 +0200

    responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,22 +10,6 @@ window.addEventListener('scroll', () => {
 
 })
 
-// Anim click
-
- window.addEventListener('click', (e) => {
-//     // console.log(e);
-
-     const rond = document.createElement('div');
-     rond.className = 'clickAnim';
-     rond.style.top = `${e.pageY - 50}px`;
-    rond.style.left = `${e.pageX - 50}px`;
-    document.body.appendChild(rond);
-
-     setTimeout(() => {
-        rond.remove();
-     }, 1500)
- })
-
 
 // Animation input
 
@@ -41,19 +25,5 @@ validationInput.addEventListener('input', (e) => {
 
 })
 
-var granimInstance = new Granim({
-    element: '#canvas-basic',
-    direction: 'left-right',
-    isPausedWhenNotInView: true,
-    states : {
-        "default-state": {
-            gradients: [
-                ['#ff9966', '#ff5e62'],
-                ['#00F260', '#0575E6'],
-                ['#e1eec3', '#f05053']
-            ]
-        }
-    }
-});
 
 
