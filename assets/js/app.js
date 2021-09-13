9fb61e89a0fd1993db7c80dc15f0195e10cc1747
BulantLucas <85559824+BulantLucas@users.noreply.github.com>
2021-09-13 11:44:09 +0200

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,3 @@
-const navigation = document.querySelector('nav');
-
-window.addEventListener('scroll', () => {
-
-     if(window.scrollY > 900){
-         navigation.classList.add('anim-nav');
-     } else {
-         navigation.classList.remove('anim-nav');
-     }
-
+window.addEventListener("cookieAlertAccept", function() {
+    
 })
-
-
-// Animation input
-
-const validationInput = document.querySelector('input');
-
-validationInput.addEventListener('input', (e) => {
-
-    if(e.target.value.length >= 3) {
-        validationInput.style.borderColor = "green";
-    } else {
-        validationInput.style.borderColor = "red";
-    }
-
-})
-
-
-
-
