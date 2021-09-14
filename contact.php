71459647466f38f16eb634f364efe3beb2898c76
Lucas <85559824+BulantLucas@users.noreply.github.com>
2021-09-14 09:24:44 +0200

    Add files via upload

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@ if(isset($_POST['envoyer'])) { // si le bouton "Envoyer" est appuyé
         echo "Le champ mail est vide";
     } else {
         //on vérifie que l'adresse est correcte
-        if(!preg_match("#^[a-z0-9_-]+((\.[a-z0-9_-]+){1,})?@[a-z0-9_-]+((\.[a-z0-9_-]+){1,})?\.[a-z]{2,}$#i",$_POST['mail'])){
+        if(!preg_match("#^[a-z0-9_-]+((\.[a-z0-9_-]+){1,})?@[a-z0-9_-]+((\.[a-z0-9_-]+){1,})?\.[a-z]{2,}$#i",$_POST['email'])){
             echo "L'adresse mail entrée est incorrecte";
         }else{
             //on vérifie que le champ sujet est correctement rempli
@@ -22,10 +22,10 @@ if(isset($_POST['envoyer'])) { // si le bouton "Envoyer" est appuyé
                     //on renseigne les entêtes de la fonction mail de PHP
                     $Entetes = "MIME-Version: 1.0\r\n";
                     $Entetes .= "Content-type: text/html; charset=UTF-8\r\n";
-                    $Entetes .= "From: Nom de votre site <".$_POST['mail'].">\r\n";//de préférence une adresse avec le même domaine de là où, vous utilisez ce code, cela permet un envoie quasi certain jusqu'au destinataire
-                    $Entetes .= "Reply-To: Nom de votre site <".$_POST['mail'].">\r\n";
+                    $Entetes .= "From: Nom de votre site <".$_POST['bulant.lucas@gmail.com'].">\r\n";//de préférence une adresse avec le même domaine de là où, vous utilisez ce code, cela permet un envoie quasi certain jusqu'au destinataire
+                    $Entetes .= "Reply-To: Nom de votre site <".$_POST['bulant.lucas@gmail.com'].">\r\n";
                     //on prépare les champs:
-                    $Mail=$_POST['mail']; 
+                    $Mail=$_POST['email']; 
                     $Sujet='=?UTF-8?B?'.base64_encode($_POST['sujet']).'?=';//Cet encodage (base64_encode) est fait pour permettre aux informations binaires d'être manipulées par les systèmes qui ne gèrent pas correctement les 8 bits (=?UTF-8?B? est une norme afin de transmettre correctement les caractères de la chaine)
                     $Message=htmlentities($_POST['message'],ENT_QUOTES,"UTF-8");//htmlentities() converti tous les accents en entités HTML, ENT_QUOTES Convertit en + les guillemets doubles et les guillemets simples, en entités HTML
                     //en fin, on envoi le mail
